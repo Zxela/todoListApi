d0374d99d6f00cfbcbd4b867df890fe9d4c772fa
Alex Zvaniga <alex.zvaniga@gmail.com>
2019-09-18 15:09:18 -0400

    completed skeleton

--- server.js
+++ server.js
@@ -1,7 +1,28 @@
 const express = require('express'),
     app = express(),
-    port = process.env.PORT || 3000;
+    port = process.env.PORT || 3000,
+    mongoose = require('mongoose'),
+    Task = require('./api/models/todoListModel'), //import models
+    bodyParser = require('body-parser');
+
+// Mongoose instance url connection
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/Tododb');
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
+
+app.use(function (req, res) {
+    res.status(404).send({
+        url: req.originalUrl + ' not found'
+    })
+})
+
+var routes = require('./api/routes/todoListRoutes'); //import routes
+routes(app); //register the routes
 
 app.listen(port);
 
-console.log('todo list RESTful API server started on : ' + port);
+console.log('todo list RESTful API server started on port: ' + port);
